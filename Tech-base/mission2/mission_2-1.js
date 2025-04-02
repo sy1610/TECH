9efc9566970bd1ddc55b0e94f275ec3c129e01a4
sy1610 <sy1610@outlook.jp>
2025-04-03 00:07:35 +0900

    Add files via upload

---
+++ Tech-base/mission2/mission_2-1.js
@@ -0,0 +1,9 @@
+var object = document.getElementById("greeting");
+var text = object.innerText;
+alert(text);
+
+
+
+//document.getElementById("id名")は、「document」（htmlのこと）の中にあるタグで「id名」に当てはまるものを取得します。//
+//（「.」は日本語の「の」、getは「取得」、Elementは「要素」、ByIDは「ID名によって」といったイメージ//
+//object.innerTextは、object（ここでは、上記のid名で取得したタグ）のテキストを取得します。（innerTextは「中のテキスト」)//
